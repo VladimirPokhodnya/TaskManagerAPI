8ee9d575a3eaf7e8e6fce8d1957d47984e9b64d4
VlagimirPokhodnya <pokhodnya-ngs.ru@yandex.ru>
2024-08-07 19:49:47 +0700

    init

---
+++ src/main/java/com/github/vladimirpokhodnya/authjwtapi/ApiForAuthenticationAndJwtIssuanceApplication.java
@@ -0,0 +1,13 @@
+package com.github.vladimirpokhodnya.authjwtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForAuthenticationAndJwtIssuanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForAuthenticationAndJwtIssuanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vladimirpokhodnya/authjwtapi/ApiForAuthenticationAndJwtIssuanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vladimirpokhodnya.authjwtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForAuthenticationAndJwtIssuanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
